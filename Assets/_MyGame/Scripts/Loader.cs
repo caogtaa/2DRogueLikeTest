ef8bd270f5d6fab993b65f5418307dc8e16ed6f5
caogtaa <caogtaa@caogtaas-MacBook-Pro.local>
2018-09-23 23:45:01 +0800

    init code from Unity

---
+++ Assets/_MyGame/Scripts/Loader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MyGame
+{
+    public class Loader : MonoBehaviour
+    {
+
+        public GameObject gameManagerPrefab;          //GameManager prefab to instantiate.
+        public GameObject soundManager;         //SoundManager prefab to instantiate.
+
+        void Awake()
+        {
+            if (GameManager.instance == null) {
+                Instantiate(gameManagerPrefab);
+            }
+
+            //Check if a SoundManager has already been assigned to static variable GameManager.instance or if it's still null
+            /*if (SoundManager.instance == null)
+            {
+                //Instantiate SoundManager prefab
+                Instantiate(soundManager);
+            }*/
+        }
+
+    }
+
+}
